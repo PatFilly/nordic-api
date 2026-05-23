f948dbf9880de404f8f16fa6e722aa1827179f6a
PatFilly <80628959+PatFilly@users.noreply.github.com>
2026-05-23 22:56:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -63,4 +63,6 @@ app.get("/api/games/open", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server läuft auf Port 3000 🚀"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT} 🚀`));
